2cd96ed91be8484950434c93fc99502b155716eb
Emeric <emeric.millot1@outlook.fr>
2024-10-17 11:02:16 +0200

    initalisation de projet

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',   // Le fichier js principal de notre code source.
+  mode: 'development',
+  output: {
+    filename: 'main.js',    // Le nom du bundle à générer
+    path: path.resolve(__dirname, 'dist'),   // Le chemin dans lequel le bundle doit être généré
+  },
+  module: {
+    rules: [
+        {
+        test: /.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+  devServer: {open: true},
+};
